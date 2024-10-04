d08740df975471c35ae59c6cefc8414947cfeaf3
Fred_Rice <fredjrice@outlook.com>
2024-10-04 16:14:34 +0100

    second commit

---
+++ error.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+function getEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 == 0) {
+            evenNumbers.push(numbers[i]);
+        }
+    }
+    return evenNumbers;
+}
+
+console.log(getEvenNumbers())
